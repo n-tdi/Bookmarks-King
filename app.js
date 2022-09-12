e39a029a25df36c7bfc381b5c66e3b06eeefdb61
professional-tdi <yogikngyt@gmail.com>
2022-09-12 12:05:51 -0400

    owow

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const text = document.querySelector(".title--text");
+const searchText = document.querySelector("#searchbar");
+
 const phrases = Array(
     "Welcome back King",
     "Good to see you",
@@ -18,5 +20,26 @@ const phrases = Array(
 let changeText = () => {
     text.innerHTML = phrases[Math.floor(Math.random()*phrases.length)];
 };
+
+const repl = (str) => {
+    str = "https://www.google.com/search?q=" + str
+        // .replace(" ", "+")
+        .replace("#", "%23")
+        .replace("\"", "%22")
+        .replace("<", "%3C")
+        .replace(">", "%3E")
+        .replace("%", "%25")
+        .replace("?", "%3F")
+        .replace(":", "%3A")
+        .replace(";", "%3B")
+        .replace("@", "%40")
+        .replace("&", "%26")
+        .replace("=", "%3D")
+        .replace("+", "%2B")
+        .replace("$", "%24")
+        .replace(",", "%2C")
+        .replace("|", "%7C");
+    return str;
+}
 
 changeText();
